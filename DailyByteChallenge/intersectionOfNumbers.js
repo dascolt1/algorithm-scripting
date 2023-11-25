7c48fc774844c9c70d6dbc77ff8da498e8b8d017
dascolt1 <dascolt1@tcnj.edu>
2023-11-25 12:24:25 -0500

    intersection of arrays

---
+++ DailyByteChallenge/intersectionOfNumbers.js
@@ -0,0 +1,17 @@
+//functionn asked by google that returns all the elements that are dhared by the 2 arrays
+//nums1 = [2, 4, 4, 2], nums2 = [2, 4], return [2, 4]
+//nums1 = [1, 2, 3, 3], nums2 = [3, 3], return [3]
+//nums1 = [2, 4, 6, 8], nums2 = [1, 3, 5, 7], return []
+
+function intersectionOfNumbers(nums1, nums2) {
+	const map = new Map();
+	let answer = []
+
+	for(i in nums1) {
+		map.set(nums1[i], "")
+	}
+
+	for(i in nums2){
+		if()
+	}
+}
