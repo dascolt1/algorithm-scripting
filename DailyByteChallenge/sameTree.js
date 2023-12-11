b0ae54a9c9e55def54fc2448c05cf866e6d0224b
dascolt1 <dascolt1@tcnj.edu>
2023-12-11 10:47:40 -0500

    same binary tree

---
+++ DailyByteChallenge/sameTree.js
@@ -0,0 +1,11 @@
+//function to check if 2 binary trees are the same
+
+function isSameTree(p, q) {
+    if(p === null && q === null) return true;
+
+    if(p === null && q != null || p != null && q == null) return false;
+
+    if(p.val != q.val) return false;
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
